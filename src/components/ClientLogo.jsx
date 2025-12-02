eb152a0e4a0f3389bb47401edcaf69663a6dffd7
Fu Jun <fulin.fujun@outlook.es>
2025-12-03 00:26:59 +0100

    Add client logo and client title components for the TPV interface

---
+++ src/components/ClientLogo.jsx
@@ -0,0 +1,11 @@
+export default function ClientLogo() {
+  return (
+    <div id="client-logo">
+      <img
+        src="/iestacio_logo.png"
+        alt="ies estació logo"
+        className="w-[100px] h-20 xl:w-[153px] xl:h-[100px]"
+      />
+    </div>
+  );
+}
---
+++ src/components/ClientTitle.jsx
@@ -0,0 +1,7 @@
+export default function ClientTitle() {
+  return (
+    <h1 className="text-center font-semibold text-lg xl:text-2xl">
+      Cafeteria L'Estació
+    </h1>
+  );
+}
